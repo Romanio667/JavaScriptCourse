3dc26a41fa4bf11cd8e40661cc87d81f1f3240f8
Roman Miroshnichenko <roma11092000@gmail.com>
2022-09-28 16:44:54 +0700

    initial

---
+++ 1/js/scpirt.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+const person = "Alex";
+const bool = true;
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.age);
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+console.log(arr[1]);
+console.log(number);
+
+const answers = [];
+answers[0]= prompt('Name','');
+answers[1]= prompt('SName','');
+answers[2]= prompt('Age','');
+document.write(answers);
+
+const category='toys';
+console.log(`https://someurl.com/${category}/5`);
