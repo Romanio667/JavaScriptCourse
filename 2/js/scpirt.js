2a8efbadd88eb0069c88b575c09966ab3ebaca47
Roman Miroshnichenko <roma11092000@gmail.com>
2022-09-29 18:40:54 +0700

    add 4

--- 2/js/scpirt.js
+++ 2/js/scpirt.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('How mane films you have watched', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How mane films have you watched', '');
+
+    while (numberOfFilms =='' || numberOfFilms == null || isNaN(numberOfFilms) ) {
+        numberOfFilms = +prompt('How mane films you have watched', '');
+    }
+}
+
+start();
 
 const personalMovieDB={
     count:numberOfFilms,
@@ -10,9 +20,45 @@ const personalMovieDB={
     private: false
 };
 
-for (let i=0; i<2; i++) {
-    const a = prompt('What film have you watched last', ''),
-    b=prompt('Your assessment', '');
-    personalMovieDB.movies[a]=b;
+
+
+function rememberMyFims() {
+    for (let i=0; i<2; i++) {
+        const a = prompt('What film have you watched last', ''),
+        b=prompt('Your assessment', '');
+        if(a!=null && b!=null && a!= '' && b!= '' && a.length < 50){
+            personalMovieDB.movies[a]=b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
+
+rememberMyFims();
+
+function writeYourGenres() {
+    for (let i=1;i<=3;i++) {
+        const genre = prompt(`Your favorite genre ${i}`);
+        personalMovieDB.genres[i-1]=genre;
+    }
+}
+
+writeYourGenres();
+
 console.log(personalMovieDB);
+ const str = "test";
+ console.log(str.toUpperCase());
+ console.log(str);
+
+ const fruit = "some fruit";
+ console.log(fruit.indexOf("fruit"));
+
+ const logg = "Hello world";
+ console.log(logg.slice(6, 11));
+ console.log(logg.substring(6, 11));
+ console.log(logg.substr(6,5));
+
+ const test="12.2px";
+ console.log(parseInt(test));
