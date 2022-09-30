d8d236d7348265f19a810697291c77928a1aa3fb
Roman Miroshnichenko <roma11092000@gmail.com>
2022-09-30 18:16:12 +0700

    change items on page

--- 8/js/script.js
+++ 8/js/script.js
@@ -14,3 +14,17 @@ hearts.forEach(item => {
     console.log(item);
 });
 
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+
+box.style.backgroundColor='blue';
+box.style.width = '500px';
+
+box.style.cssText='background-color: blue; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor='red';
+
+for(let i=0; i<hearts.length; i++){
+    hearts[i].style.backgroundColor='blue';
+}
