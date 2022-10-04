cfb8f772c573713dcf05dde753831613cd281d8e
Roman Miroshnichenko <roma11092000@gmail.com>
2022-10-04 21:40:52 +0700

    add 9 lesson

--- 8/js/script.js
+++ 8/js/script.js
@@ -1,20 +1,14 @@
 'use strict';
 
 const box = document.getElementById('box');
-console.log(box);
-
 const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
-
 const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
 const hearts = document.querySelectorAll('.heart');
 hearts.forEach(item => {
     console.log(item);
 });
-
 const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
 console.log(oneHeart);
 
 box.style.backgroundColor='blue';
@@ -28,3 +22,20 @@ circles[0].style.backgroundColor='red';
 for(let i=0; i<hearts.length; i++){
     hearts[i].style.backgroundColor='blue';
 }
+
+hearts.forEach(item => {
+    item.style.backgroundColor='blue';
+});
+
+const div = document.createElement('div');
+const text = document.createTextNode('I was here');
+
+const div2 = document.createElement('div');
+div2.classList.add('black');
+document.body.append(div2);
+wrapper.append(div2);//after
+wrapper.prepend(div2);//before
+hearts[0].before(div2);
+hearts[0].after(div2);
+circles[0].remove();
+hearts[0].replaceWith(circles[0]);
